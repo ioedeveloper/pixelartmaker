428b6aa38219964f86a1c02a6dfb6f27936934f6
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-06-25 15:54:25 +0100

    Create Grid

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
-// Select color input
 // Select size input
+var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(e){
+    color = $('#colorPicker').val();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid();
+    e.preventDefault();
+    });
 
 function makeGrid() {
+    // Your code goes here!
+    var canvas = $('#pixelCanvas');
+    var row = "<tr></tr>";
+    var col = "<td></td>";
+    
+    canvas.children().remove();
+    for(var i=0; i<height; i++){
+        canvas.append(row);
+    }
+    var rows = $('tr');
+    for(var j=0; j<width; j++){
+        rows.append(col);
+    }
 
-// Your code goes here!
-
+    cell = canvas.find('td');
+    $(this).attr('bgcolor',color);
 }
