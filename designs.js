e8b3538abdecc2b97d26c8bada088ca89ba7cd59
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-06-27 13:52:55 +0100

    Comments

--- designs.js
+++ designs.js
@@ -1,8 +1,8 @@
-// Select size input
+// Variable declarations
 var height, width, color, cell, isClicked;
 var canvas = $('#pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, assign variables and call makeGrid()
 $('#sizePicker').submit(function(e){
     color = $('#colorPicker').val();
     height = $('#inputHeight').val();
@@ -11,10 +11,12 @@ $('#sizePicker').submit(function(e){
     e.preventDefault();
 });
 
+// Assign color
 $('#colorPicker').on('change', function(){
     color = $(this).val();
 });
 
+// Handles creation of grid
 function makeGrid() {
     var row = "<tr></tr>";
     var col = "<td></td>";
@@ -33,14 +35,24 @@ function makeGrid() {
     });
     
     cell = canvas.find('td');
+
+    // Listens to cell click event and paints cell 
     cell.click(function(){
         $(this).attr('bgcolor',color);
-        if(isClicked) isClicked = false;
-        else isClicked=true;
+
+        if(isClicked){
+            isClicked = false;
+        }
+        else {
+            isClicked=true;
+        }
        
     });
 
+    // Listens to cell hover event and paints cell if isClicked condition is true
     cell.hover(function(){
-        if(isClicked) $(this).attr('bgcolor',color);
+        if(isClicked){
+            $(this).attr('bgcolor',color);
+        }
     });
 }
